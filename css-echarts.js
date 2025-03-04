9b8fca337a216aa550c680d4f19f9ffb39ec32f5
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-03-03 21:53:32 -0500

    Initial commit

--- css-echarts.js
+++ css-echarts.js
@@ -2,7 +2,7 @@
 import { CSSCharts } from 'css-charts/css-charts.js';
 import { config } from './config.js';
 
-/** @import {AllProps, Actions, PAP, DataItem} from  './ts-refs/css-echarts/types' */
+/** @import {AllProps, Actions} from  './ts-refs/css-echarts/types' */
 /** @import {MntCfg, MountProps, MountActions, ITransformer} from './ts-refs/trans-render/types' */
 
 /**
